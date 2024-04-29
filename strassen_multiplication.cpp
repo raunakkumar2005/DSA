e26013086c848d449ed9d618283112d0fcfe70c1
raunak kumar <117460745+raunakkumar2005@users.noreply.github.com>
2024-04-29 10:13:16 +0530

    Update strassen_multiplication.cpp

--- strassen_multiplication.cpp
+++ strassen_multiplication.cpp
@@ -1,41 +1,44 @@
 #include <iostream>
 using namespace std;
 
-// Function to multiply two 2x2 matrices
-void multiply2x2(int A[2][2], int B[2][2], int C[2][2]) {
-    int a = A[0][0], b = A[0][1], c = A[1][0], d = A[1][1];
-    int e = B[0][0], f = B[0][1], g = B[1][0], h = B[1][1];
+// Function to perform 2x2 matrix multiplication using Strassen algorithm
+void strassenMultiplication(int A[2][2], int B[2][2], int C[2][2]) {
+    int M1 = (A[0][0] + A[1][1]) * (B[0][0] + B[1][1]);
+    int M2 = (A[1][0] + A[1][1]) * B[0][0];
+    int M3 = A[0][0] * (B[0][1] - B[1][1]);
+    int M4 = A[1][1] * (B[1][0] - B[0][0]);
+    int M5 = (A[0][0] + A[0][1]) * B[1][1];
+    int M6 = (A[1][0] - A[0][0]) * (B[0][0] + B[0][1]);
+    int M7 = (A[0][1] - A[1][1]) * (B[1][0] + B[1][1]);
 
-    C[0][0] = a * e + b * g;
-    C[0][1] = a * f + b * h;
-    C[1][0] = c * e + d * g;
-    C[1][1] = c * f + d * h;
+    C[0][0] = M1 + M4 - M5 + M7;
+    C[0][1] = M3 + M5;
+    C[1][0] = M2 + M4;
+    C[1][1] = M1 - M2 + M3 + M6;
 }
 
 int main() {
     int A[2][2], B[2][2], C[2][2];
 
     cout << "Enter the elements of matrix A (2x2):" << endl;
-    for (int i = 0; i < 2; ++i) {
-        for (int j = 0; j < 2; ++j) {
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 2; j++) {
             cin >> A[i][j];
         }
     }
 
     cout << "Enter the elements of matrix B (2x2):" << endl;
-    for (int i = 0; i < 2; ++i) {
-        for (int j = 0; j < 2; ++j) {
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 2; j++) {
             cin >> B[i][j];
         }
     }
 
-    // Compute matrix multiplication
-    multiply2x2(A, B, C);
+    strassenMultiplication(A, B, C);
 
-    // Print the result matrix
-    cout << "Result of matrix multiplication:" << endl;
-    for (int i = 0; i < 2; ++i) {
-        for (int j = 0; j < 2; ++j) {
+    cout << "Result of multiplication:" << endl;
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 2; j++) {
             cout << C[i][j] << " ";
         }
         cout << endl;
